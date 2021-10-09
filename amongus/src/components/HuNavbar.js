1618efe2a644b0729f5837736203977ff8971755
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-10-09 12:21:10 -0300

    first component and page but with errors

---
+++ amongus/src/components/HuNavbar.js
@@ -0,0 +1,17 @@
+import React, from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+const HuNavbar = () => {
+    return (
+    <>
+        <Navbar bg="light" expand="lg">
+                <Container>
+                </Container>
+        </Navbar>
+    </>
+    );
+}; 
+
+export default HuNavbar;
---
+++ amongus/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, from 'react';
+
+import { HuNavbar } from '../components/HuNavbar';
+
+const Home = () => {
+    return (
+        <HuNavbar />
+    );
+};
+
+export default Home;
