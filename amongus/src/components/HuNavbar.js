d154ccf417e85713a4813df2a416c1491b2ec70b
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-10-09 13:15:45 -0300

    changes to HuNavbar

--- amongus/src/components/HuNavbar.js
+++ amongus/src/components/HuNavbar.js
@@ -5,12 +5,13 @@ import Navbar from 'react-bootstrap/Navbar';
 
 const HuNavbar = () => {
     return (
-    
+        <>
         <Navbar bg="light" expand="lg">
                 <Container>
+                    <p>Hola</p>
                 </Container>
         </Navbar>
-   
+        </>
     );
 }; 
 
