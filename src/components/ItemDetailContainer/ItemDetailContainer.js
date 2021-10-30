ebff65565565606f59fdd68440e54b6e3c83745a
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-10-30 11:53:16 -0300

    ItemDetailContainer first version. It must be improved.

---
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import productData from '../data/data.js';
+import ItemDetail from './ItemDetail/ItemDetail';
+
+const ItemDetailContainer = (props) => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect( () => {
+        const productList = new Promise( (resolve, reject) => {
+            setTimeout(() => {
+                resolve(productData)
+            }, 3000); //3000 ms Delay 
+        });
+        productList.then( (result) => {
+            setProducts(productData) //Promise to obtain products
+            setLoading(false)
+        });
+        },
+        [products] //It is executed when products change
+    );
+    return (
+        <>
+        <h1 className="text-center mt-4">{props.title}</h1>
+        {loading ? <h2 className="text-center">Cargando productos</h2> : <ItemDetail products={products}/>}
+        </>
+    );
+};
+
+export default ItemDetailContainer;
--- src/components/data/data.js
+++ src/components/data/data.js
@@ -4,6 +4,7 @@ const productData = [
         productImg: "../assets/img/imp-box-2.jpg",
         productTitle: "Juego de mesa Among Us",
         productDescription: "2 a 10 jugadores",
+        productLongDescription: "¡Alerta, hay un impostor entre nosotros! Estás en la nave espacial junto a otros participantes. ¡Pero cuidado!. Uno de ustedes es el impostor e intentará de cualquier manera quitar del medio al resto de la tripulación. ¿Podran detenerlo? Necesitarás un dado, diez muñecos 3D Among Us y un mazo de cartas. Usando estas herramientas y siguiendo las reglas de juego, podrás guiar a tus tripulantes a cumplir misiones o a los impostores a aniquilarlos. ¿Estás listo para llevar a cabo tu misión?",
         productPrice: 3350
     },
     {
@@ -11,6 +12,7 @@ const productData = [
         productImg: "../assets/img/imp-cup.png",
         productTitle: "Taza Among Us",
         productDescription: "Modelos a elección",
+        productLongDescription: "¡Toma tus infusiones o jugos en tu taza favorita! Ideal para llevar al colegio o jardín. Es de plástico no se calienta al tener una bebida con temperatura.",
         productPrice: 850
     },
     {
@@ -18,6 +20,7 @@ const productData = [
         productImg: "../assets/img/imp-figures.jpg",
         productTitle: "3 muñecos 3D Among Us",
         productDescription: "Colores a elección",
+        productLongDescription: "¿Querés agregar más jugadores a tu juego de mesa? ¿Simplemente querés jugar a tus propias aventuras? Con estos 3 muñecos de 3cm de altura, puedes jugar a abrirlos en 2 y simular el videojuego.",
         productPrice: 650
     }
 ];
