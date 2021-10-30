76810a1e3db38a7f1986da6f1bf9b318e806c44f
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-10-30 13:13:34 -0300

    Change label for buttons in Item

--- src/components/ItemListContainer/itemList/item/Item.js
+++ src/components/ItemListContainer/itemList/item/Item.js
@@ -12,7 +12,7 @@ const Item = ( { productImg, productTitle, productDescription, productPrice } ) 
                 <Card.Title>{productTitle}</Card.Title>
                 <Card.Text>{productDescription}</Card.Text>
                 <Card.Text>{productPrice}</Card.Text>
-                <Button className='hu-button-active btn btn-primary btn-lg' variant="primary">Comprar</Button>
+                <Button className='hu-button-active btn btn-primary btn-lg' variant="primary">Ver más</Button>
             </Card.Body>
         </Card>
         </>
