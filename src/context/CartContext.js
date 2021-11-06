c162d5ac943c74f293bafd97745ea7c41870c25e
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-11-06 14:00:37 -0300

    Update CartContext.js

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,64 +1,22 @@
-import React, { useContext, createContext, useState } from "react";
+import React, { useState } from 'react';
 
-//creacion del contexto
-const CartContext = createContext();
+export const CartContext = React.createContext({});
 
-//funcion para ahorrarme dos importaciones
-export const useCartContext = () => useContext(CartContext);
+export default function CartContext ({
+    children,
+}) {
+    const [darkMode, setDarkMode] = React.useState(false);
+    const [products, setProducts] = React.useState([]);
 
-//Inyectar los estados y funciones globales, enmascaro provider
-export const CartContextProvider = ({ children }) => {
-    let variable = "desde context";
-    const [cart, setCart] = useState([]);
-  
-    const addItem = (item, quantity) => {
-      const index = cart.findIndex((i) => i.item.id === item.id);
-      if (index > -1) {
-        const oldQy = cart[index].quantity;
-        cart.splice(index, 1);
-        setCart([...cart, { item, quantity: quantity + oldQy }]);
-      } else {
-        setCart([...cart, { item, quantity }]);
-      }
-    };
-  
-    const clearCart = () => {
-      setCart([]);
-    };
-  
-    const removeItem = (id) => {
-      const deleteProduct = cart.filter((prod) => prod.item.id !== id);
-      setCart([...deleteProduct]);
-    };
-  
-    //[1,2,3,4] acum = 0 => 1, 1+2 => 3+3
-    const cartWidgetItems = () => {
-      return cart.reduce((acum, valor) => acum + valor.quantity, 0);
-      // return cart.length;
-    };
-  
-    const totalPrice = () => {
-      return cart.reduce(
-        (acum, valor) => acum + valor.quantity * valor.item.price,
-        0
-      );
-    };
-  
     return (
-      <CartContext.Provider
-        value={{
-          variable,
-          cart,
-          setCart,
-          addItem,
-          clearCart,
-          removeItem,
-          cartWidgetItems,
-          totalPrice,
-        }}
-      >
-        {children}
-      </CartContext.Provider>
+        <CartContext.Provider value={{
+            darkMode,
+            setDarkMode,
+            products,
+            setProducts
+        }}>
+            {children}
+        </CartContext.Provider>
     );
-  };
-  
+
+}
