7a12c74b2d60ce76c00de53644919b90732e3bc7
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-11-11 17:34:11 -0300

    link to Cart from NavBar

--- src/components/HuNavbar/HuCartWidget/HuCartWidget.js
+++ src/components/HuNavbar/HuCartWidget/HuCartWidget.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { NavLink, Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 
 import '../HuNavbar.css';
@@ -7,13 +7,15 @@ import '../HuNavbar.css';
 const HuCartWidget = () => {
     return (
         <Nav.Link className="hu-navbar-option pr-1">
-            Mi carrito
-            <img
-                alt="Cart"
-                src="../assets/icons/cart.svg"
-                width="30px"
-                height="auto"
-            />
+            <NavLink to='/cart' >
+                Mi carrito
+                <img
+                    alt="Cart"
+                    src="../assets/icons/cart.svg"
+                    width="30px"
+                    height="auto"
+                />
+            </NavLink>
         </Nav.Link>
     );
 }; 
