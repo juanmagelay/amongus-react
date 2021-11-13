c65305033f8d0458ea8377e6ca42c146bb2671fe
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-11-13 13:28:39 -0300

    changes to firebase

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -1,7 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -28,5 +31,5 @@ const getFirestoreDb = () => {
 
 export {
     app,
-    getFirestoreDb,
+    getFirestoreDb
 }
