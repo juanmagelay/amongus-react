0afe1cf4c150f430950e7cd01f8712e323aaedd7
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-11-19 16:38:22 -0300

    firebase implementation

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
 
 import { productData } from '../data/data.js';
-import { ItemDetail } from './ItemDetail/ItemDetail'; 
+import { ItemDetail } from './ItemDetail/ItemDetail';
+
+//Import de db que creamos nosotros
+import db from '../../lib/firebaseConfig';
+
+//imports para traer UN solo dato
+import { doc, getDoc } from 'firebase/firestore';
 
 export const ItemDetailContainer = () => {
     const [item, setItem] = useState({});
     const { id } = useParams();
 
     useEffect(() => {
+        /*
+        DATA LOCAL
         const getItems = new Promise((resolve) => {
             setTimeout(() => {
                 resolve(productData);
@@ -18,7 +27,16 @@ export const ItemDetailContainer = () => {
         getItems.then((res) => {
             setItem(res.find((i) => i.id === id));
           });
-        }, [id]);
+        */
+        const getItems = doc(db, 'productData', id);
+    
+        // aca usamos el paso intermedio de crear un objeto para agregarle el id de firebase, que viene por afuera de nuestro objeto
+        getDoc(getItems)
+        .then((res) => {
+            const result = { id: res.id, ...res.data() };
+            setItem(result);
+        })
+        }, []);
     
     return <ItemDetail {...item} />;
 };
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -5,22 +5,48 @@ import Spinner from 'react-bootstrap/Spinner';
 import { productData } from '../data/data.js';
 import { ItemList } from './itemList/ItemList';
 
+//Import de db que creamos nosotros
+import db from '../../lib/firebaseConfig';
+
+//Import de las funciones para traer colecciones, muchos datos
+import { collection, query, where, getDocs } from 'firebase/firestore';
+
+
 export const ItemListContainer = ( { greeting } ) => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
     const { catId } = useParams();
 
     useEffect( () => {
-        const productList = new Promise( (resolve) => {
-            setTimeout(() => {
-                resolve(productData);
-            }, 3000); //3000 ms Delay 
-        });
-        
-        productList.then( (res) => {
-            catId ? setProducts(res.filter((i) => i.category === catId)) : setProducts(res);
-            setLoading(false)
-        });
+        /*
+        DATA LOCAL
+            const productList = new Promise( (resolve) => {
+                setTimeout(() => {
+                    resolve(productData);
+                }, 3000); //3000 ms Delay 
+            });
+            
+            productList.then( (res) => {
+                catId ? setProducts(res.filter((i) => i.category === catId)) : setProducts(res);
+                setLoading(false)
+            });
+        */
+        // DATA DE FIREBASE
+        setLoading(true);
+
+        const productList = catId
+            ? query(collection(db, 'productData'), where('category', '==', catId))
+            : collection(db, 'productData');
+  
+        // Usamos el paso intermedio de crear un objeto para agregarle el id de firebase, que viene por afuera de nuestro objeto
+        getDocs(productList)
+            .then((res) => {
+                const results = res.docs.map((doc) => {
+                return { ...doc.data(), id: doc.id };
+            });
+            setProducts(results);
+            })
+            .finally(() => setLoading(false));
     }, [catId]);
     
     return (
