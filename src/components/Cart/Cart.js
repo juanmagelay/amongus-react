17d7d5e8c38553053d3f6d4ef75d5691cad25c19
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2021-11-20 10:23:13 -0300

    Empty cart UI and back to home

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React, {useContext} from "react";
 import { Context } from "../../context/CartContext";
+import { Link } from "react-router-dom"
 
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
@@ -32,12 +33,27 @@ const Cart = ()=>{
                 )}
                 <Card className="w-90 justify-content-between align-items-center shadow m-2">
                     <Row className="justify-content-between align-items-center p-2">
-                        <Col xs={12} md={10}>
-                            <h5 className="pt-2">El total del carrito es <span className="lead">{'$' + total.toLocaleString("es-AR")}</span> y tenés {unidades} productos</h5>
-                        </Col>
-                        <Col className="text-center" xs={12} md={2}>
-                            <Button variant=" hu-button-active btn btn-primary btn-lg">Pagar</Button>
-                        </Col>
+                        { unidades>0 ?
+                            <>
+                            <Col xs={12} md={10}>
+                                <h5 className="pt-2">El total del carrito es <span className="lead">{'$' + total.toLocaleString("es-AR")}</span> y tenés {unidades} productos</h5>
+                            </Col>
+                            <Col className="text-center" xs={12} md={2}>
+                                <Button variant=" hu-button-active btn btn-primary btn-lg">Pagar</Button>
+                            </Col>
+                            </>
+                        :
+                            <>
+                            <Col xs={12} md={8}>
+                                <h5 className="pt-2">El carrito esta vacio, empezá a comprar</h5>
+                            </Col>
+                            <Col className="text-center" xs={12} md={4}>
+                                <Link to='/'>
+                                    <Button variant=" hu-button-active btn btn-primary btn-lg">Ver productos</Button>
+                                </Link>
+                            </Col>
+                            </> 
+                        }
                     </Row>
                 </Card>
         </Container>
